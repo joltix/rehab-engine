751ce6122303665aa0a0f2dfdba41a00e19fd03c
Trish Dao Le <TrishDaoLe@Trishs-MacBook-Pro.local>
2016-02-16 16:46:53 -0500

    Sprite class
    added many methods to easily set and get sprite properties

--- src/com/rehab/animation/Sprite.java
+++ src/com/rehab/animation/Sprite.java
@@ -1,9 +1,62 @@
 package com.rehab.animation;
 
+import java.lang.Object;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+
 public class Sprite {
+	// a wrapper for the BufferImage
+	// mirror image method
+	// store bufferimage as class memeber
+	private WritableImage buffer;
+	// private GraphicsContext gpu;
+
+	private int id;
+
+	private Sprite(WritableImage b) {
+		// store the image
+
+		buffer = b;
+	}
+
+	public WritableImage mirror(WritableImage image) {
+		PixelReader pixel = image.getPixelReader();
+		
+		for (int i = 0; i < image.getWidth(); i++) {
+			
+			for (int j = 0; j < image.getHeight(); j++) {
+				pixel.getArgb(i, j);
+
+			}
+
+		}
+		return image;
+
+	}
+
+	public void draw(GraphicsContext g, int x, int y) {
+		g.drawImage(buffer, x, y);
+
+	}
+
+	public int getId() {
+
+		return id;
+	}
+
+	public void setId(int i) {
+
+		id = i;
+	}
 	
-	public void draw(){
+	public double getHeight(){
+		return buffer.getHeight(); 
 		
+	}
+	
+	public double getWidth(){
 		
+		return buffer.getWidth(); 
 	}
 }
