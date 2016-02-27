1ad34b9fbdf048b0be3a3aca017145366a7e9e27
Joltix <chrisramos343@gmail.com>
2016-02-27 14:13:44 -0500

    Update for physics revision
    Removed applyForce() for moving physics to Phys class
    Removed jump()
    Changed gravity from a scalar to a Vector
    Changed coordinate system to use origin as the bottom left
    Added applyGravity()

--- src/com/rehab/world/Arena.java
+++ src/com/rehab/world/Arena.java
@@ -1,7 +1,9 @@
 package com.rehab.world;
 
+import com.rehab.world.Phys.Vector;
+
 public class Arena {
-	
+
 	// Playable bounds on-screen
 	private double mWidth, mHeight;
 	// Entities to keep track of (those in the level)
@@ -9,14 +11,13 @@ public class Arena {
 	// Title of the level
 	private String mName;
 	// Default gravity is at Earth level
-	private double mGrav = 9.8
+	private Vector mGrav;
 
 	/**
 	 * Prevents default instantiation
 	 */
-	@SuppressWarnings("unused")
-	private Arena() {  }
-	
+	@SuppressWarnings("unused") private Arena() {  }
+
 	/**
 	 * Constructor for a basic Arena.
 	 * @param name
@@ -30,42 +31,47 @@ public class Arena {
 		mName = name;
 		mWidth = width;
 		mHeight = height;
+
+		// Setup gravity Vector
+		double gravStrength = Physics.getPlanetGravity(Physics.EARTH_MASS, Physics.EARTH_RADIUS);
+		mGrav = new Vector();
+		mGrav.updateFrom(Phys.UNIT_SOUTH);
+		mGrav.changeMagnitude(gravStrength);
 	}
 
 	/**
-	 * Calculates the level's current game state. This includes instance
-	 * locations, health and damage dealing, etc.
+	 * [INCOMPLETE] Calculates the level's current game state. This includes instance locations, health
+	 * and damage dealing, etc. In its current form, this method should only be used to
+	 * test various reactions and / or functions and their values in a one-shot manner.
+	 * Once an Entity reaches the floor of the Arena (getHeight() = 0), nothing more can
+	 * be said of what will occur.
 	 */
 	public void step() {
 		for (Entity e : mEntities) {
-			
+
 			// Apply gravity
-			if (e.hasContact()) applyForce(e, mGrav * e.getMass(), 270, 1d);
-			
+			applyGravity(e);
+
 			// Snap the character to the surface of the floor if sinks
 			if (isBelowScreen(e)) {
-				e.moveTo(e.getX(), mHeight - e.getHeight());
-				e.setContact(true);
+				e.moveTo(e.getX(), 0);
+				e.setEnableGravity(false);
 			}
-			
+
 		}
 	}
-	
-	private void applyForce(Entity e, double force, double direction, double timeScale) {
-		double acceleration = force / e.getMass();
-		e.setSpeed(e.getSpeed() + (acceleration * timeScale));
-		double currentSpeed = e.getSpeed();
-		
-		// Figure next location
-		double newX = Physics.atDistanceWithAngleX(direction, currentSpeed);
-		double newY = Physics.atDistanceWithAngleY(direction, currentSpeed);
-		e.moveBy(newX, newY);
+
+	/**
+	 * Applies the Arena's gravity vector to a given Entity.
+	 * @param e
+	 * 		the Entity to affect.
+	 */
+	private void applyGravity(Entity e) {
+		Vector force = new Vector();
+		force.updateFrom(mGrav);
+		e.moveImpulse(force);
 	}
-	
-	private void jump(Entity e) {
-		applyForce(e, e.getMass() * 10, 90, 1d);
-	}
-	
+
 	/**
 	 * Checks whether or not an Entity is below the screen.
 	 * @param e
@@ -76,35 +82,52 @@ public class Arena {
 	private boolean isBelowScreen(Entity e) {
 		double y = e.getY();
 		double height = e.getHeight();
-		return (y + height) > mHeight;
+		return (y + height) < 0;
 	}
-	
+
 	/**
 	 * Gets the title of the arena.
 	 * @return
 	 * 		the title.
 	 */
 	public String getName() { return mName; }
-	
+
 	/**
 	 * Gets the magnitude of acceleration due to gravity for the particular level.
 	 * @return
-	 * 		the magnitude of gravity in meters per second squared.
+	 * 		a Vector representing gravity in meters per second squared.
 	 */
-	public double gravity() { return mGrav; }
-	
+	public Vector getGravity() { return mGrav; }
+
 	/**
 	 * Sets the Arena's level of gravity.
 	 * @param gravity
 	 * 		the magnitude of gravity in meters per second squared.
 	 */
-	public void setGravity(double gravity) { mGrav = gravity; }
-	
+	public void setGravity(double gravity) {
+		if (mGrav == null) mGrav = new Vector();
+		mGrav = new Vector();
+		mGrav.updateFrom(Phys.UNIT_SOUTH);
+	}
+
 	/**
 	 * Sets the Entities meant to appear in the level.
 	 * @param it
 	 * 		an Iterable of Entities in the level.
 	 */
 	public void setEntities(Iterable<Entity> iter) { mEntities = iter; }
+
+	/**
+	 * Gets the width of the playable area.
+	 * @return
+	 * 		the Arena's width boundary.
+	 */
+	public double getWidth() { return mWidth; }
 	
+	/**
+	 * Gets the height of the playable area.
+	 * @return
+	 * 		the Arena's height boundary.
+	 */
+	public double getHeight() { return mHeight; }
 }
