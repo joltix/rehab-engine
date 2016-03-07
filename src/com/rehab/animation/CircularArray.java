741fb295a61b2827a62825040927da18ae857c1b
TrishDaoLe <trishle9@gmail.com>
2016-03-07 16:44:31 -0500

    fix the issue of negative

--- src/com/rehab/animation/CircularArray.java
+++ src/com/rehab/animation/CircularArray.java
@@ -9,13 +9,14 @@ public class CircularArray<E> extends ArrayList<E> {
 		// TODO Auto-generated method stub
 		//if the index we want to get is less than zero
 		//then set index to be index+size
-		int size = size();
-		
-		if(index < 0) {  index = index + size;}
-		// Index too big to handle so wrap around
-		else if (index >= size index = index % size;
+				int size = size();
+				int abs = Math.abs(index+size); 
+				//index too small will wrap itself back around
+				if(index < 0) {  index = abs % size;}
+				// Index too big to handle so wrap around
+				else if (index >= size) index = index % size;
 
-		return super.get(index);
+				return super.get(index);
 	}
 	
 	public static void main(String [ ] args){
