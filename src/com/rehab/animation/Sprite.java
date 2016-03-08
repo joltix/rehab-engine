d302ad412a3eb5b55c92fac1b68bcd4774d21e24
Joltix <chrisramos343@gmail.com>
2016-03-07 19:51:32 -0500

    Changed drawing method
    Changed draw() to directly draw onto a buffer’s PixelWriter (for buffer
    drawing) instead of a Canvas’ GraphicsContext

--- src/com/rehab/animation/Sprite.java
+++ src/com/rehab/animation/Sprite.java
@@ -1,8 +1,8 @@
 package com.rehab.animation;
 
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
 
 public class Sprite {
 
@@ -40,15 +40,18 @@ public class Sprite {
 		return image;
 
 	}
-	
-	public void draw(GraphicsContext g, double x, double y) { draw(g, x, y, true); }
-
-	public void draw(GraphicsContext g, double x, double y, boolean convertCoords) {
-		// Convert to standard cartesian
-		if (convertCoords) y = 480 - y;
-		g.drawImage(buffer, x, y);
+		
+	public void draw(PixelWriter writer, int offsetX, int offsetY) {
+		PixelReader reader = buffer.getPixelReader();
+		
+		// Transfer pixels to the buffer's writer
+		for (int x = 0, w = (int) getWidth(); x < w; x++)
+			for (int y = 0, h = (int) getHeight(); y < h; y++)
+				writer.setArgb(offsetX + x, offsetY + y, reader.getArgb(x, y));
 		
 	}
+	
+	public PixelReader getPixelReader() { return buffer.getPixelReader(); }
 
 	public void setId(int i) { id = i; }
 	
