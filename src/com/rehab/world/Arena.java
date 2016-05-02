e58e96ed06f1a72f9f7209f4993f0eea921e6829
Joltix <chrisramos343@gmail.com>
2016-05-02 15:50:16 -0400

    Updated gravity implementation and added support for Props
    Changed gravity to use Entity's new move() method
    Moved the Arena's floor to be set instead of calling the Main
    
    Added support for Props and their collision

--- src/com/rehab/world/Arena.java
+++ src/com/rehab/world/Arena.java
@@ -11,40 +11,46 @@ public class Arena {
 	// Title of the level
 	private String mName;
 	// Default gravity is at Earth level
-	private Vector2D mGrav = new Vector2D(0, 0);
+	private Vector2D mGravDirection = new Vector2D(Vector2D.UNIT_SOUTH);
+	private double mGravMagnitude = 0;
 
 	// Entities to keep track of (those in the level)
 	private Iterable<Actor> mActList;
 	private Iterable<Projectile> mProjList;
+	private Iterable<Prop> mPropList;
+	
+	
 	private Actor mPlayer;
-
+	private Prop mFloor;
+	
 	/**
 	 * Constructor for a basic Arena.
-	 * @param name
-	 * 		a title for the level.
-	 * @param width
-	 * 		the playable area's width.
-	 * @param height
-	 * 		the playable area's height.
+	 * 
+	 * @param name	a title for the level.
+	 * @param width	the playable area's width.
+	 * @param height	the playable area's height.
+	 * @param floor	Prop representing the game floor.
 	 */
-	public Arena(String name, double width, double height) {
+	public Arena(String name, double width, double height, Prop floor) {
 		mName = name;
 		mWidth = width;
 		mHeight = height;
+		mFloor = floor;
 
 		// Setup gravity Vector
-		double gravStrength = Physics.getPlanetGravity(Physics.EARTH_MASS, Physics.EARTH_RADIUS);
-		mGrav.updateFrom(Vector2D.UNIT_SOUTH);
-		mGrav.changeMagnitude(gravStrength);
+		mGravMagnitude = Physics.getPlanetGravity(Physics.EARTH_MASS, Physics.EARTH_RADIUS);
+		mGravDirection.changeMagnitude(mGravMagnitude);
 	}
 	
 	/**
 	 * Sets the unit of time to use for Entity physics.
-	 * @param tickRate
-	 * 		a fraction of a second.
+	 * 
+	 * @param tickRate	a fraction of a second.
 	 */
-	public void setDeltaTime(double tickRate) {
+	public void setTimescale(double tickRate) {
+		
 		mUnitTime = tickRate;
+		// Let all Phys instances know the scale for computations
 		Phys.syncTimescale(mUnitTime);
 	}
 
@@ -57,14 +63,6 @@ public class Arena {
 	 */
 	public void stepActors() {
 		for (Actor a : mActList) {
-
-			// Print dummy location
-//			if (a.getId() == 2) {
-//				System.out.printf("Dummy location (%f, %f)\n", a.getX(), a.getY());
-//				for (Vector2D edge : a.getCollision().edges()) {
-//					System.out.printf("    Edge: %s\n", edge);
-//				}
-//			}
 			
 			// Disable objects beyond the screen
 			if (isOutside(a)) {
@@ -74,21 +72,19 @@ public class Arena {
 			
 			// Apply gravity
 			if (a.isGravityEnabled()) {
-				applyGravity(a);
+				
+				a.move();
+				
 				for (Entity other : mActList) {
 					// Skip collision with itself
 					if (a == other) continue;
-//					if (a.collidesWith(other)) {
-//						System.out.printf("Actor collision: [%d] (%f, %f) with [%d]\n", a.getId(), a.getX(), a.getY(), other.getId());
-//						// Snap object to floor
-//						if (other == Main.getFloor()) {
-//							
-//							snapToFloor(a);
-//							a.setEnableGravity(false);
-//						}
-//						//a.moveTo(a.getX(), other.getY() + a.getHeight());
-//						//a.setEnableGravity(false);
-//					}
+					if (a.collidesWith(other)) {
+						System.out.printf("Actor collision: [%d] (%f, %f) with [%d]\n", a.getId(), a.getX(), a.getY(), other.getId());
+						// Snap object to floor
+						if (other == mFloor) {
+							snapToFloor(a);
+						}
+					}
 				}
  			}
 			
@@ -98,13 +94,20 @@ public class Arena {
 				a.setEnableGravity(false);
 				System.out.printf("Actor below screen: %s\n", a);
 			}
-
+			
 		}
 	}
 	
+	/**
+	 * Moves the Actor to the surface of the Arena's floor.
+	 * 
+	 * @param a	Actor to relocate.
+	 */
 	private void snapToFloor(Actor a) {
-		Actor floor = Main.getFloor();
-		a.moveTo(a.getX(), a.getHeight() + floor.getHeight());
+		double locationY = a.getHeight() + mFloor.getHeight();
+		System.out.printf("Snapping to location Y %f with actor height(%f) and floor height (%f)\n", locationY, a.getHeight(), mFloor.getHeight());
+		a.moveTo(a.getX(), locationY);
+		a.setEnableGravity(false);
 	}
 
 	public void stepProjectiles() {
@@ -121,24 +124,28 @@ public class Arena {
 				p.disable();
 			} else {
 				
-				// Test collision with all Actor and disable projectile
+				// Test collision with all Actors
 				boolean collides = false;
 				for (Actor a : mActList) {
-					
-					if (a != Main.getFloor() && (collides = p.collidesWith(a))) {
-						System.out.printf("Projectile[%d] collision with [%d]! Disabling Projectile %s\n", p.getId(), a.getId(), p);
+					if (collides = p.collidesWith(a)) {
+						System.out.printf("Projectile(%d) collision with Actor(%d)! Disabling Projectile %s\n", p.getId(), a.getId(), p);
 						p.disable();
 						break;
 					}
 				}
 				
+				// Test collision with all Props
+				for (Prop prop : mPropList) {
+					if ((prop != mFloor) && (collides = p.collidesWith(prop))) {
+						System.out.printf("Projectile(%d) collision with Prop(%d)! Disabling Projectile %s\n", p.getId(), prop.getId(), p);
+						p.disable();
+						break;
+					}					
+				}
+				
 				// Move again only if no collision
 				if (!collides) {
-					Vector2D velo = p.getPhysics().getVelocity();
-					Vector2D direction = velo.getUnitVector();
-					//direction.multiply(p.getSpeed());
 					p.move();
-					//p.moveBy(1, 1);
 				}
 			}
 			
@@ -146,20 +153,10 @@ public class Arena {
 	}
 	
 	/**
-	 * Applies the Arena's gravity vector to a given Entity.
-	 * 
-	 * @param e	the Entity to affect.
-	 */
-	private void applyGravity(Entity e) {
-		e.moveBy(0, -2);
-	}
-
-	/**
 	 * Checks whether or not an Entity is below the Arena's boundaries.
-	 * @param e
-	 * 		the Entity whose location must be checked.
-	 * @return
-	 * 		true if the Entity is below the Arena, false otherwise.
+	 * 
+	 * @param e	the Entity whose location must be checked.
+	 * @return true if the Entity is below the Arena, false otherwise.
 	 */
 	private boolean isBelow(Entity e) {
 		double y = e.getY();
@@ -169,10 +166,9 @@ public class Arena {
 	
 	/**
 	 * Checks whether or not a given Entity is beyond the Arena's boundaries.
-	 * @param e
-	 * 		the Entity to check for.
-	 * @return
-	 * 		true if the Entity is outside the Arena, false otherwise.
+	 * 
+	 * @param e	the Entity to check for.
+	 * @return	true if the Entity is outside the Arena, false otherwise.
 	 */
 	private boolean isOutside(Entity e) {
 		double x = e.getX(), y = e.getY();
@@ -185,67 +181,56 @@ public class Arena {
 
 	/**
 	 * Gets the title of the arena.
-	 * @return
-	 * 		the title.
+	 * 
+	 * @return the title.
 	 */
 	public String getName() { return mName; }
 
 	/**
-	 * Gets the magnitude of acceleration due to gravity for the particular level.
-	 * @return
-	 * 		a Vector2D representing gravity in meters per second squared.
-	 */
-	public Vector2D getGravity() {
-		return mGrav;
-	}
-
-	/**
-	 * Sets the Arena's level of gravity.
-	 * @param gravity
-	 * 		the magnitude of gravity in meters per second squared.
-	 */
-	public void setGravity(double gravity) {
-		mGrav.changeMagnitude(gravity);
-	}
-
-	/**
 	 * Sets the Entities meant to appear in the level.
-	 * @param acts
-	 * 		an Iterable of Actors.
-	 * @param projs
-	 * 		an Iterable of Projectiles.
+	 * 
+	 * @param acts	an Iterable of Actors.
+	 * @param projs	an Iterable of Projectiles.
+	 * @param props an Iterable of Props.
 	 */
-	public void setEntities(Iterable<Actor> acts, Iterable<Projectile> projs)
-	{
+	public void setEntities(Iterable<Actor> acts, Iterable<Projectile> projs, Iterable<Prop> props) {
 		mActList = acts;
 		mProjList = projs;
+		mPropList = props;
+		
+		// Set gravity for each Actor
+		for (Actor a : mActList) {
+			Phys phys = a.getPhysics();
+			phys.setAcceleration(mGravMagnitude);
+			phys.setVelocity(mGravDirection.getX(), mGravDirection.getY(), 1);
+		}
 	}
 	
 	/**
 	 * Gets the user-controlled Actor.
-	 * @return
-	 * 		the Actor representing the user.
+	 * 
+	 * @return the Actor representing the user.
 	 */
 	public Actor getPlayer() { return mPlayer; }
 	
 	/**
 	 * Sets the Actor to be controlled by the user.
-	 * @param player
-	 * 		the user's Actor.
+	 * 
+	 * @param player	the user's Actor.
 	 */
 	public void setPlayer(Actor player) { mPlayer = player; }
 	
 	/**
 	 * Gets the width of the playable area.
-	 * @return
-	 * 		the Arena's width boundary.
+	 * 
+	 * @return the Arena's width boundary.
 	 */
 	public double getWidth() { return mWidth; }
 	
 	/**
 	 * Gets the height of the playable area.
-	 * @return
-	 * 		the Arena's height boundary.
+	 * 
+	 * @return the Arena's height boundary.
 	 */
 	public double getHeight() { return mHeight; }
 }
