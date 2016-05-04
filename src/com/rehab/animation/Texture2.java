a98416eaf7a589260925d6947fe998fce0ab114f
TrishDaoLe <trishle9@gmail.com>
2016-05-04 16:51:48 -0400

    updated

--- src/com/rehab/animation/Texture2.java
+++ src/com/rehab/animation/Texture2.java
@@ -13,48 +13,87 @@ public class Texture2 {
 	private int width;
 	private int height; 
 	private int comp;
-	private int id; 
+	ByteBuffer image;
+	ByteBuffer imageBuffer;
+	IntBuffer w ;
+    IntBuffer h ;
+    IntBuffer c ;
+	//add a method to get all of the files from a folder, i.e. for loop get names and put that as the paramter
 	
-	public int loadTexture(String filename){
-	    ByteBuffer imageBuffer;
-	    try{
-	        imageBuffer = readFile(filename);
-	    }
-	    catch (IOException e) {
-	        throw new RuntimeException(e);
-	    }
+	public Texture2 (String filename){
+		 
+		    try{
+		        imageBuffer = readFile(filename);
+		    }
+		    catch (IOException e) {
+		        throw new RuntimeException(e);
+		    }
+		    
+		    loadTexture(filename);
+	}
 	
-	    IntBuffer w = BufferUtils.createIntBuffer(1);
-	    IntBuffer h = BufferUtils.createIntBuffer(1);
-	    IntBuffer comp = BufferUtils.createIntBuffer(1);
-
-	    ByteBuffer image = STBImage.stbi_load_from_memory(imageBuffer, w, h, comp, 0);
+	public int getWidth(){
+		return  width = w.get(0);
+	}
+	
+	public int getHeight(){
+		return  height = h.get(0);
+	}
+	
+	public int getComp(){
+		return comp = c.get(0);
+	}
+	
+	public void rotate(float angle, float x, float y){
+		GL11.glRotatef(angle, x, y, 1);
+	}
+	private int loadTexture(String filename){
+	
+		w = BufferUtils.createIntBuffer(1);
+	     h = BufferUtils.createIntBuffer(1);
+	    c = BufferUtils.createIntBuffer(1);
+	    
+	     image = STBImage.stbi_load_from_memory(imageBuffer, w, h, c, 0);
+	    //if can not open the file then throw an error
 	    if(image == null){
 	        throw new RuntimeException("Failed to load image: " + STBImage.stbi_failure_reason());
 	    }
-
-	    this.width = w.get(0);
-	    this.height = h.get(0);
-	    this.comp = comp.get(0);
-
-	    if(this.comp == 3){
-	        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, this.width, this.height, 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, image);
+	    
+	    if(getComp() == 3){
+	    	//function specifies a two-dimensional texture image
+	        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, getWidth(), getHeight(), 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, image);
 	    }
 	    else{
-	        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, this.width, this.height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, image);
+	        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, getWidth(), getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, image);
 
-	        GL11.glEnable(GL11.GL_BLEND);
+	        GL11.glEnable(GL11.GL_BLEND);//functions enable  OpenGL capabilities
 	        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 	    }
-
+	    //Sets texture parameters, needs a target and valued texture parameter
 	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
 	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-
+	    //enable texture 2d
 	    GL11.glEnable(GL11.GL_TEXTURE_2D);
-
+	 
+	    
+	    // function generates texture names
 	    return GL11.glGenTextures();
 	}
-
+	
+	
+public 	ByteBuffer getByteBuffer (){
+	
+	return image;
+}
+	
+/**
+ * Helper method that reads in a file specifying a filename 
+ * 
+ * @param resource
+ * @return
+ * ByteBuffer
+ * @throws IOException
+ */
 	private ByteBuffer readFile(String resource) throws IOException{
 	    File file = new File(resource);
 
