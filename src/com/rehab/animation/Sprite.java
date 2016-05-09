6409907d6b59132a0e3cb87f180a8080716c53af
TrishDaoLe <trishle9@gmail.com>
2016-05-08 23:27:28 -0400

    updated Sprite, manager and texture class
    updated the internal to handle OpenGL stuff and textures

--- src/com/rehab/animation/Sprite.java
+++ src/com/rehab/animation/Sprite.java
@@ -31,27 +31,42 @@ import javafx.scene.image.WritableImage;
  * drawable object
  * 
  */
-
+//move the draw code into the byte buffer 
 public class Sprite {
-	//private WritableImage buffer; 
+
 	// instance of the ID
-	private int id;
+	private String mFileName;
 	// list or array that can hold multiple reels, {hashtable}
-	private Hashtable<String, CircularArray<Image>> reelsNum;
+	private Hashtable<String, CircularArray<Texture2>> mReelsNum;
 	// hold the images for the animation
-	private CircularArray<Image> reels = new CircularArray<Image>();
-	private long startTime;
-	private long localStartTime;
-	private long seconds;
-	boolean isFirstPic = false;
-	private AnimationDatabase data;
-	private Image[] divergent = new Image[2];
-	private static final int IMAGE_RIGHT = 0; 
-	private static final int  IMAGE_LEFT = 1; 
-	Texture2 tex;
-	
+	private CircularArray<Texture2> mReels;
+	private long mStartTime;
+	private long mLocalStartTime;
+	private long mSeconds;
+	boolean mIsFirstPic = false;
+	private AnimationDatabase mData;
+	Texture2 mTex;
+
 	@SuppressWarnings("unused")
 	private Sprite() {
+	}
+
+	/**
+	 * Constructor for loading an image from a file. The file must be within the
+	 * same directory as the Sprite.java
+	 * 
+	 * @param fileName
+	 *            the name of the image with file type.
+	 */
+	@SuppressWarnings("unused")
+	public Sprite(String fileName) {
+		
+		String path = Sprite.class.getResource(fileName).getPath();
+		//create the texture from the specified filename 
+		mTex = new Texture2(path); 		
+		//add the sprite into the database each time we create one 
+		mData = new AnimationDatabase();
+	
 	}
 
 	/**
@@ -61,11 +76,12 @@ public class Sprite {
 	 * @param b
 	 * @return false if it can not add, true otherwise
 	 */
-	public boolean add(String r, CircularArray<Image> b) {
-		if (reelsNum.contains(r)) {
+	public boolean add(String r, CircularArray<Texture2> b) {
+		if (mReelsNum.contains(r)) {
 			return false;
+
 		} else {
-			reelsNum.put(r, b);
+			mReelsNum.put(r, b);
 			return true;
 		}
 	}
@@ -78,53 +94,77 @@ public class Sprite {
 	 * @return the interval in miliseconds
 	 */
 	public double getInterval(double seconds) {
-		int size = reels.size();
+		int size = mReels.size();
 		long mili = (long) (seconds * 1000);
 		return size / mili;
 	}
 
 	/**
-	 * Constructor for loading an image from a file. The file must be within the
-	 * same directory as the Sprite.java
+	 * Gets the pixel data making the image to be shown on the canvas.
 	 * 
-	 * @param fileName
-	 *            the name of the image with file type.
+	 * @param animation	the name of the animation to draw.
+	 * @param imgIndex	the index of the image within an animation.
+	 * @return a ByteBuffer of pixels to draw.
 	 */
-	@SuppressWarnings("unused")
-	public Sprite(String fileName) {
-//		int width =(int) divergent[IMAGE_RIGHT].getWidth();
-//		int height = (int)divergent[IMAGE_RIGHT].getHeight();
-		// store the image
-		//Image img = new Image(Sprite.class.getResourceAsStream(fileName));//index0 facing to the right
-		//WritableImage mImg = new WritableImage(width, height);//index1
-		//buffer=(WritableImage) img;
-//		divergent[IMAGE_RIGHT]=img; 
-//		divergent[IMAGE_LEFT]=mImg; 
-		tex = new Texture2(fileName);
-		data = new AnimationDatabase();
+	public ByteBuffer getByteBuffer(String animation, int imgIndex){
 		
-	}
-	
-//	public void rotateObj(float ang, float x, float y){
-//		tex.rotate(ang, x, y);
-//		
-//	}
-	
-	public ByteBuffer getByteBuffer(){
-		 return tex.getByteBuffer();
-		
-	}
-	
-	public int getWidth(){
-		return tex.getWidth();
-	}
+		//get the arr assoc with the name
+		CircularArray<Texture2> arr = mData.getTexArr(animation);  
 
-	public int getHeight(){
-		return tex.getHeight();
+		long currentTime = System.currentTimeMillis();
+
+		long duration = currentTime - mLocalStartTime;
+		//if this is the first pic and the array size is one
+		if (mIsFirstPic = true && arr.size() == 1) { 	
+			//then don't animate 
+			mStartTime = 0;										
+
+		}
+
+		// if start is equal or greater than the seconds then stop
+		if (duration >= getInterval(mSeconds) && mStartTime != 0) {		
+
+			mLocalStartTime = System.currentTimeMillis();
+
+			//get the reel array of the given animation
+			CircularArray<Texture2> updatePic = mData.getTexArr(animation);
+			//get that specific index from the texture array
+			Texture2 draw = updatePic.get(imgIndex);
+			mTex = draw; 
+			mIsFirstPic = true;
+		}
+		// Get ByteBuffer from the selected Texture
+		return mTex.getByteBuffer();
+
+
 	}
-	
+	public ByteBuffer getByteBuffer(){
+		return mTex.getByteBuffer();
+		
+	}
+	/**
+	 * Getter for the width of the texture we already loaded 
+	 * 
+	 * @return width of the texture
+	 */
+	public int getWidth(){
+		return mTex.getWidth();
+	}
+	/**
+	 * Getter for the Height of the texture 
+	 * 
+	 * @return int height 
+	 */
+	public int getHeight(){
+		return mTex.getHeight();
+	}
+	/**
+	 * Getter for the transparency of the texture 
+	 * 
+	 * @return int comp
+	 */
 	public int getComp(){
-		return tex.getComp();
+		return mTex.getComp();
 	}
 	/**
 	 * To start the timer for animation
@@ -132,245 +172,30 @@ public class Sprite {
 	 * @return the start time
 	 */
 	public long startAnime() {
-		startTime = System.currentTimeMillis();
-		localStartTime = startTime;
-		isFirstPic = false;
-		return startTime;
+		mStartTime = System.currentTimeMillis();
 
+		mLocalStartTime = mStartTime;
+
+		mIsFirstPic = false;
+
+		return mStartTime;
 	}
 
-	/**
-	 * Draw the object that is being given
-	 * 
-	 * @param writer
-	 * @param offsetX
-	 * @param offsetY
-	 */
-//	public void draw(PixelWriter writer, int offsetX, int offsetY) {
-//		//int getPicNum = 0;
-//
-//		long currentTime = System.currentTimeMillis();
-//		long duration = currentTime - localStartTime;
-//
-//		if (isFirstPic = true && buffer == reels.get(0)) {
-//			startTime = 0;
-//			//buffer = reels.get(0);
-//		}
-//
-//		// if start is equal or greater than the seconds then stop
-//		if (duration >= getInterval(seconds) && startTime != 0) {
-//			localStartTime = System.currentTimeMillis();
-//			//getPicNum++;
-//			//buffer = reels.get(getPicNum);
-//			isFirstPic = true;
-//		}
-//
-//		// Transfer pixels to the buffer's writer
-//		//PixelReader reader = buffer.getPixelReader();
-//		for (int x = 0, w = (int) getWidth(); x < w; x++) {
-//			for (int y = 0, h = (int) getHeight(); y < h; y++) {
-//				int realX = offsetX + x, realY = offsetY + y;
-//				// Draw the pixels only if its within the screen
-//				if (realX >= 0 && realX < 720 && realY >= 0 && realY < 480)
-//					writer.setArgb(offsetX + x, offsetY + y, reader.getArgb(x, y));
-//			}
-//		}
-//
-//	}
-
-	
-//	private int[] getPixel(Image img) {
-//		PixelReader target = img.getPixelReader();
-//		int col = getImageHeight(img);// int j
-//		int row = getImageWidth(img);// int i
-//		int[] pixel1D = new int[row * col];
-//
-//		for (int i = 0; i < row; i++) {
-//			for (int j = 0; j < col; j++) {
-//				pixel1D[i * j] = target.getArgb(i, j);
-//			}
-//		}
-//		return pixel1D;
-//
-//	}
-
-//	public int getImageWidth(Image i) {
-//		i = divergent[IMAGE_RIGHT];
-//
-//		int width = (int) i.getWidth();
-//		return width;
-//	}
-//
-//	public int getImageHeight(Image i) {
-//		i = divergent[IMAGE_RIGHT];
-//
-//		int height = (int) i.getHeight();
-//		return height;
-
-	//}// call this method to reverse the pixel given the pixel array
-
-//	private int[] mirror(int pixels[], int width, int height) {
-//		int newPixels[] = null;
-//		newPixels = new int[width * height];
-//		int index = 0;
-//		// Loop forwards through all the rows
-//		for (int y = 0; y < height; y++)
-//			// for each row loop backwards through the columns
-//			for (int x = width - 1; x >= 0; x--) {
-//				newPixels[index] = pixels[y * width + x];
-//				index++;
-//			}
-//		return newPixels;
-//	}
-	//call the two helper methods mirror pixel to 
-	//reverse the image
-//	public WritableImage reverse(Image imi){
-//		int [] img = getPixel(imi); //pixel array from the buffer
-//		int width= getImageWidth(imi);
-//		int height = getImageHeight(imi);
-		
-//		PixelWriter writer = buffer.getPixelWriter(); //the writer
-		//loop through the array and draw them onto an writable image
-//		for(int i=0; i<width;i++){
-//			for(int j=0; j<height;j++){
-//				writer.setArgb(i, j,writer. );//no idea what to pass as the third parameter
-//				
-//			}
-//		}
-//		int[] almostThere = mirror(img, width, height);//get a 1D array 
-//		//get a number form the array to a setARB
-//		
-//		WritableImage k = new WritableImage(width, height);  
-//		PixelWriter pk = k.getPixelWriter();
-//		 for(int i =0; i<width; i++){
-//			 for(int j=0; j<height; j++){
-//				//go thru 1d and get all the pixel and draw 
-//				 pk.setArgb(i, j, almostThere[i*height+j] );
-//			 }
-//		 }
-//		return k;
-//		 
-//		
-//		
-//		
-//	}
-	//draw method for the reverse images. 
-	//given the matrix need to write the images onto the canvas
-//	public Image drawRev (){
-//		
-//		return buffer;
-//		
-//		
-//	}
-
-	// given an angle rotate the image in that manner
-//	public int[] getRotational(double angle, int[] pixels, int width, int height) {
-//		final double radians = Math.toRadians(angle); //convert to radian
-//		final double cos = Math.cos(radians);
-//		final double sin = Math.sin(radians);
-//		final int[] pixels2 = new int[pixels.length];
-//		for (int pixel = 0; pixel < pixels2.length; pixel++) {
-//
-//			pixels2[pixel] = 0xFFFFFF;
-//
-//		}
-//		for (int x = 0; x < width; x++) {
-//			for (int y = 0; y < height; y++) {
-//				final int centerx = width / 2;
-//				final int centery = height / 2;
-//				final int m = x - centerx;
-//				final int n = y - centery;
-//				final int j = ((int) (m * cos + n * sin)) + centerx;
-//				final int k = ((int) (n * cos - m * sin)) + centery;
-//				if (j >= 0 && j < width && k >= 0 && k < height) {
-//					pixels2[(y * width + x)] = pixels[(k * width + j)];
-//
-//				}
-//			}
-//		}
-//
-//		return pixels2;
-//
-//	}
-
-	/**
-	 * Calling the PixelReader on a Image
-	 * 
-	 * @return the Pixel reader for reading the pixel of the image
-	 */
-//	public PixelReader getPixelReader() {
-//		return buffer.getPixelReader();
-//	}
 
 	/**
 	 * Method to set the ID for Sprite
 	 * 
 	 * @param i
 	 */
-	public void setId(int i) {
-		id = i;
-	}
+	public void setId(String i) {	mFileName = i;	}
+
 
 	/**
 	 * Getting the ID of a specific Sprite
 	 * 
 	 * @return the ID
 	 */
-	public int getId() {
-		return id;
-	}
+	public String getId() {	return mFileName;	}
 
-	/**
-	 * Get the Height the image
-	 * 
-	 * @return the value in which is the height
-	 */
-//	public double getHeight() {
-//		return buffer.getHeight();
-//	}
-//
-//	/**
-//	 * Get the Width of the image
-//	 * 
-//	 * @return the value of which is the width
-//	 */
-//	public double getWidth() {
-//		return buffer.getWidth();
-//	}
-	
-
-	/**
-	 * Method to get and retrieve the files in a given Folder
-	 * 
-	 * @param folder
-	 */
-//	public ArrayList<BufferedImage> getFolder(final File folder){
-//		File[] files = folder.listFiles(); //array of objs
-//		ArrayList<BufferedImage> image = new ArrayList<BufferedImage>();
-//		
-//		
-//		for(int i=0; i<files.length;i++){//reading in files from the array of files
-//			if (files[i].isFile()){//if this is a file then add the image to the arraylist
-//				try {
-//					image.add(i,ImageIO.read(files[i]));//this gives me a bufferedImage
-//				} catch (IOException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//			}
-//			
-//		}
-//		
-//		 return image;//return the arraylist of BufferedImages
-//		
-//	}
-
-//	public WritableImage getBuffer(){
-//		
-//		return buffer; 
-//	}
-//	public void setImage(WritableImage i){
-//		buffer =i; 
-//	}
 
 }
--- src/com/rehab/animation/Texture2.java
+++ src/com/rehab/animation/Texture2.java
@@ -1,114 +1,145 @@
 package com.rehab.animation;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.stb.STBImage;
+
+import javafx.scene.image.Image;
+
 public class Texture2 {
-	private int width;
-	private int height; 
-	private int comp;
-	ByteBuffer image;
-	ByteBuffer imageBuffer;
-	IntBuffer w ;
-    IntBuffer h ;
-    IntBuffer c ;
-	//add a method to get all of the files from a folder, i.e. for loop get names and put that as the paramter
-	
-	public Texture2 (String filename){
-		 
-		    try{
-		        imageBuffer = readFile(filename);
-		    }
-		    catch (IOException e) {
-		        throw new RuntimeException(e);
-		    }
-		    
-		    loadTexture(filename);
-	}
-	
-	public int getWidth(){
-		return  width = w.get(0);
-	}
-	
-	public int getHeight(){
-		return  height = h.get(0);
-	}
-	
-	public int getComp(){
-		return comp = c.get(0);
-	}
-	
-	public void rotate(float angle, float x, float y){
-		GL11.glRotatef(angle, x, y, 1);
-	}
-	private int loadTexture(String filename){
-	
-		w = BufferUtils.createIntBuffer(1);
-	     h = BufferUtils.createIntBuffer(1);
-	    c = BufferUtils.createIntBuffer(1);
-	    
-	     image = STBImage.stbi_load_from_memory(imageBuffer, w, h, c, 0);
-	    //if can not open the file then throw an error
-	    if(image == null){
-	        throw new RuntimeException("Failed to load image: " + STBImage.stbi_failure_reason());
-	    }
-	    
-	    if(getComp() == 3){
-	    	//function specifies a two-dimensional texture image
-	        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, getWidth(), getHeight(), 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, image);
-	    }
-	    else{
-	        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, getWidth(), getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, image);
+	private int mWidth;
+	private int mHeight;
+	private int mComp;
 
-	        GL11.glEnable(GL11.GL_BLEND);//functions enable  OpenGL capabilities
-	        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-	    }
-	    //Sets texture parameters, needs a target and valued texture parameter
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-	    //enable texture 2d
-	    GL11.glEnable(GL11.GL_TEXTURE_2D);
-	 
-	    
-	    // function generates texture names
-	    return GL11.glGenTextures();
-	}
-	
-	
-public 	ByteBuffer getByteBuffer (){
-	
-	return image;
-}
-	
-/**
- * Helper method that reads in a file specifying a filename 
- * 
- * @param resource
- * @return
- * ByteBuffer
- * @throws IOException
- */
-	private ByteBuffer readFile(String resource) throws IOException{
-	    File file = new File(resource);
+	ByteBuffer mImage;
+	ByteBuffer mImageBuffer;
+	IntBuffer mW;
+	IntBuffer mH;
+	IntBuffer mC;
 
-	    FileInputStream fis = new FileInputStream(file);
-	    FileChannel fc = fis.getChannel();
+	/**
+	 * Texture Class that takes in a filename and convert to Byte Buffer
+	 * 
+	 * @param filename
+	 */
+	public Texture2(String filename) {
 
-	    ByteBuffer buffer = BufferUtils.createByteBuffer((int) fc.size() + 1);
+		try {
+			mImageBuffer = readFile(filename);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 
-	    while(fc.read(buffer) != -1);
-
-	    fis.close();
-	    fc.close();
-	    buffer.flip();
-
-	    return buffer;
+		loadTexture(filename);
 	}
 
+	
+
+	/**
+	 * Get Width method for the texture
+	 * 
+	 * @return
+	 */
+	public int getWidth() {
+		return mWidth = mW.get(0);
+	}
+
+	/**
+	 * Get Height for the texture
+	 * 
+	 * @return
+	 */
+	public int getHeight() {
+		return mHeight = mH.get(0);
+	}
+
+	/**
+	 * Get the transparency of the texture
+	 * 
+	 * @return
+	 */
+	public int getComp() {
+		return mComp = mC.get(0);
+	}
+
+	/**
+	 * Helper method to load the image file into a texture to then be drawn on
+	 * the Canvas
+	 * 
+	 * @param filename
+	 * @return an integer of the texture data
+	 */
+	private int loadTexture(String filename) {
+
+		mW = BufferUtils.createIntBuffer(1);
+		mH = BufferUtils.createIntBuffer(1);
+		mC = BufferUtils.createIntBuffer(1);
+
+		mImage = STBImage.stbi_load_from_memory(mImageBuffer, mW, mH, mC, 0);
+		// if can not open the file then throw an error
+		if (mImage == null) {
+			throw new RuntimeException("Failed to load image: " + STBImage.stbi_failure_reason());	}
+		
+
+		if (getComp() == 3) {
+			// function specifies a two-dimensional texture image
+			GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, getWidth(), getHeight(), 0, GL11.GL_RGB,
+					GL11.GL_UNSIGNED_BYTE, mImage);
+		} else {
+			GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, getWidth(), getHeight(), 0, GL11.GL_RGBA,
+					GL11.GL_UNSIGNED_BYTE, mImage);
+
+			GL11.glEnable(GL11.GL_BLEND);// functions enable OpenGL capabilities
+			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		}
+		// Sets texture parameters, needs a target and valued texture parameter
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+		// enable texture 2d
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+
+		// function generates texture names
+		return GL11.glGenTextures();
+	}
+
+	/**
+	 * Getter for the ByteBuffer from an image we want from
+	 * 
+	 * @return ByteBuffer of the image we want to load
+	 */
+	public ByteBuffer getByteBuffer() {	return mImage;	}
+	
+
+	/**
+	 * Helper method that reads in a file specifying a filename
+	 * 
+	 * @param resource
+	 * @return ByteBuffer
+	 * @throws IOException
+	 */
+	private ByteBuffer readFile(String resource) throws IOException {
+		File file = new File(resource);
+
+		FileInputStream fis = new FileInputStream(file);
+		FileChannel fc = fis.getChannel();
+
+		ByteBuffer buffer = BufferUtils.createByteBuffer((int) fc.size() + 1);
+
+		while (fc.read(buffer) != -1)	;
+
+		fis.close();
+		fc.close();
+		buffer.flip();
+
+		return buffer;
+	}
+	
 }
